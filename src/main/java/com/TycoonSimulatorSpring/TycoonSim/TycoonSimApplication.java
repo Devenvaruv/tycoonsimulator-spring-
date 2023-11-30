f36cd8b4b752f14c2591433f8d842fba7228463f
your_github_username <devenvaruv@gmail.com>
2023-11-30 14:44:10 -0800

    's'

---
+++ src/main/java/com/TycoonSimulatorSpring/TycoonSim/TycoonSimApplication.java
@@ -0,0 +1,13 @@
+package com.TycoonSimulatorSpring.TycoonSim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TycoonSimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TycoonSimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TycoonSimulatorSpring/TycoonSim/TycoonSimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TycoonSimulatorSpring.TycoonSim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TycoonSimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
